8d17e38dd4ae651009b42fea578a70045dffdc7b
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2024-02-18 16:58:45 +0300

    1.1.4

--- ishop/src/components/Item.js
+++ ishop/src/components/Item.js
@@ -3,10 +3,10 @@ import React from 'react'
 export default function Item({elem}) {
   return (
     <div className='item'>
-        <img src={''} />
+        <img src={'./img/' + elem.img} />
         <h2>{elem.title}</h2>
         <p>{elem.desc}</p>
-        <b>{elem.price}</b>
+        <b>{elem.price} руб.</b>
         <div className='add-to-cart'>+</div>
     </div>
   )
